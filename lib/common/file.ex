5911b8bc888a27160fcd606d6684fcc2822e5369
Jim Menard <jim@jimmenard.com>
2017-12-10 10:35:21 -0500

    file name format change

--- lib/common/file.ex
+++ lib/common/file.ex
@@ -1,12 +1,15 @@
 defmodule Common.File do
   @doc """
-  Given a module symbol or string, return a path to the default input file
-  for that module, relative to the root of the project.
+  Given a module atom or string, return a path to the default input file for
+  that module, relative to the root of the project.
 
   ## Examples:
 
       iex> Common.File.default_input_path(Y2016.Day08)
       "data/y2016/day_08.txt"
+
+      iex> Common.File.default_input_path(Y2016.Day08, 2)
+      "data/y2016/day_08_part2.txt"
   """
   def default_input_path(mod) do
     path =
@@ -17,7 +20,13 @@ defmodule Common.File do
       |> String.split(".")
       |> tl
       |> Enum.join("/")
-    "data/#{path}.txt"
+    "data/#{path}str}.txt"
+  end
+
+  def default_input_path(mod, part) do
+    path = default_input_path(mod)
+    part_str = if part, do: "_part#{part}", else: ""
+    path |> String.replace(".txt", "#{part_str}.txt")
   end
 
   def lines(file) do
