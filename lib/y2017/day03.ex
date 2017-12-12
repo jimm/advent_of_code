71af4c3f16961afdc8808baa9097c6ff37c17d06
Jim Menard <jim@jimmenard.com>
2017-12-12 11:25:20 -0500

    days 3 and 4

--- lib/y2017/day03.ex
+++ lib/y2017/day03.ex
@@ -1,43 +1,5 @@
 # Spiral Memory
 
-# 37 36 35 34 33 32 31
-# 38 17 16 15 14 13 30
-# 39 18  5  4  3 12 29
-# 40 19  6  1  2 11 28
-# 41 20  7  8  9 10 27
-# 42 21 22 23 24 25 26
-# 43 44 45 46 47 48 49
-
-# Part 2:
-#
-# As a stress test on the system, the programs here clear the grid and then
-# store the value 1 in square 1. Then, in the same allocation order as shown
-# above, they store the sum of the values in all adjacent squares, including
-# diagonals.
-#
-# So, the first few squares' values are chosen as follows:
-#
-# - Square 1 starts with the value 1.
-# - Square 2 has only one adjacent filled square (with value 1), so it also
-#   stores 1.
-# - Square 3 has both of the above squares as neighbors and stores the sum
-#   of their values, 2.
-# - Square 4 has all three of the aforementioned squares as neighbors and
-#   stores the sum of their values, 4.
-# - Square 5 only has the first and fourth squares as neighbors, so it gets
-#   the value 5.
-# - Once a square is written, its value does not change. Therefore, the
-#   first few squares would receive the following values:
-#
-# 147  142  133  122   59
-# 304    5    4    2   57
-# 330   10    1    1   54
-# 351   11   23   25   26
-# 362  747  806--->   ...
-#
-# What is the first value written that is larger than your puzzle input?
-
-
 defmodule Y2017.Day03 do
   import Bitwise
   alias Common.File, as: CF
@@ -49,6 +11,7 @@ defmodule Y2017.Day03 do
   end
 
   def part2 do
+    stress_test_value(2, Map.put(%{}, {0, 0}, 1))
   end
 
   # ================ part 1 ================
@@ -56,15 +19,53 @@ defmodule Y2017.Day03 do
   defp manhattan_distance_to_start, do: manhattan_distance_to(@start_location)
 
   defp manhattan_distance_to(i) do
+    {x, y} = coord_of(i)
+    abs(x) + abs(y)
+  end
+
+  # ================ part 2 ================
+
+  defp stress_test_value(i, state) do
+    coord = coord_of(i)
+    val = sum_of_surounding(coord, state)
+    if val > @start_location do
+      val
+    else
+      new_state = Map.put(state, coord, val)
+      stress_test_value(i + 1, new_state)
+    end
+  end
+
+  defp sum_of_surounding({x, y}, state) do
+    at(  x-1, y+1, state) +
+      at(x,   y+1, state) +
+      at(x+1, y+1, state) +
+      at(x-1, y,   state) +
+      at(x+1, y,   state) +
+      at(x-1, y-1, state) +
+      at(x,   y-1, state) +
+      at(x+1, y-1, state)
+  end
+
+  defp at(x, y, state) do
+    Map.get(state, {x, y}, 0)
+  end
+
+  # ================ helpers ================
+
+  defp sqrt_of_bottom_corner_containing(i) do
+    isqrt = trunc(:math.sqrt(i))
+    if band(isqrt, 1) == 1 do
+      if isqrt * isqrt == i, do: isqrt, else: isqrt+2
+    else
+      isqrt+1
+    end
+  end
+
+  defp coord_of(i) do
     sqrt = sqrt_of_bottom_corner_containing(i)
     bot_right = sqrt * sqrt
-    if bot_right == i do
-      sqrt - 1
-    else
-      mc = max_coord(sqrt)
-      {x, y} = find_coord(i, bot_right, sqrt, mc)
-      abs(x) + abs(y)
-    end
+    find_coord(i, bot_right, sqrt, max_coord(sqrt))
   end
 
   defp find_coord(i, bot_right, sqrt, mc) when i >= bot_right - (sqrt-1) do
@@ -93,13 +94,4 @@ defmodule Y2017.Day03 do
   end
 
   defp max_coord(sqrt), do: div(sqrt, 2)
-
-  defp sqrt_of_bottom_corner_containing(i) do
-    isqrt = trunc(:math.sqrt(i))
-    if band(isqrt, 1) == 1 do
-      if isqrt * isqrt == i, do: isqrt, else: isqrt+2
-    else
-      isqrt+1
-    end
-  end
 end
---
+++ lib/y2017/day04.ex
@@ -0,0 +1,52 @@
+# High-Entropy Passphrases
+
+defmodule Y2017.Day04 do
+  alias Common.File, as: CF
+
+  def part1 do
+    count_valid(&words_valid?/1)
+  end
+
+  def part2 do
+    count_valid(&sorted_words_valid?/1)
+  end
+
+  # ================ part 1 ================
+
+  defp words_valid?(line) do
+    # Need to sort for Enum.dedup
+    sorted_words =
+      line
+      |> String.split
+      |> Enum.sort
+    length(sorted_words) == length(Enum.dedup(sorted_words))
+  end
+
+  # ================ part 2 ================
+
+  defp sorted_words_valid?(line) do
+    # Need to sort for Enum.dedup
+    sorted_dorsw =
+      line
+      |> String.split
+      |> Enum.map(&sort_letters/1)
+      |> Enum.sort
+    length(sorted_dorsw) == length(Enum.dedup(sorted_dorsw))
+  end
+
+  defp sort_letters(word) do
+    word
+    |> String.split("", trim: true)
+    |> Enum.sort
+    |> Enum.join
+  end
+
+  # ================ helpers ================
+
+  defp count_valid(f) do
+    __MODULE__
+    |> CF.default_input_path
+    |> CF.lines
+    |> Enum.count(f)
+  end
+end
