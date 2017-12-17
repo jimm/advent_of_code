25126db351b2d10e3ef61a3344b29157979ab224
Jim Menard <jim@jimmenard.com>
2017-12-17 10:36:17 -0500

    day 5

---
+++ lib/y2017/day05.ex
@@ -0,0 +1,59 @@
+# A Maze of Twisty Trampolines, All Alike
+
+defmodule Y2017.Day05 do
+  alias Common.File, as: CF
+
+  def part1 do
+    steps_until_out_of_bounds(fn(offset) -> offset + 1 end)
+  end
+
+  def part2 do
+    steps_until_out_of_bounds(fn(offset) ->
+      if offset >= 3, do: offset - 1, else: offset + 1
+    end)
+  end
+
+  # ================ helpers ================
+
+  defp steps_until_out_of_bounds(f) do
+    indices = Stream.iterate(0, &(&1+1))
+    nums =
+      __MODULE__
+      |> CF.default_input_path
+      |> CF.lines
+      |> Enum.map(&String.to_integer/1)
+    m = Map.new(Enum.zip(indices, nums))
+    do_steps_until_out_of_bounds(m, length(nums), 0, 0, f)
+  end
+
+  defp do_steps_until_out_of_bounds(_, len, idx, steps, _)
+  when idx < 0 or idx >= len do
+    steps
+  end
+  defp do_steps_until_out_of_bounds(m, len, idx, steps, f) do
+    offset = m[idx]
+    new_idx = idx + offset
+    new_m = Map.replace(m, idx, f.(offset))
+    do_steps_until_out_of_bounds(new_m, len, new_idx, steps+1, f)
+  end
+
+  # defp steps_until_out_of_bounds(f) do
+  #   nums =
+  #     __MODULE__
+  #     |> CF.default_input_path
+  #     |> CF.lines
+  #     |> Enum.map(&String.to_integer/1)
+  #   do_steps_until_out_of_bounds(nums, 0, 0, 0, f)
+  # end
+
+  # defp do_steps_until_out_of_bounds(nums, idx, _, steps, _)
+  # when idx < 0 or idx >= length(nums) do
+  #   steps
+  # end
+  # defp do_steps_until_out_of_bounds(nums, idx, offset, steps, f) do
+  #   offset = Enum.at(nums, idx)
+  #   new_idx = idx + offset
+  #   new_nums = List.replace_at(nums, idx, f.(offset))
+  #   do_steps_until_out_of_bounds(new_nums, new_idx, offset, steps+1, f)
+  # end
+end
