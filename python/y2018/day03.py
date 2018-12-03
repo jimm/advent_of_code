cad1196ae3918fce5633407156351f75b5e53eae
Jim Menard <jim@jimmenard.com>
2018-12-03 07:18:50 -0500

    day 3

---
+++ python/y2018/day03.py
@@ -0,0 +1,63 @@
+# No Matter How You Slice It
+
+from utils import data_file_lines
+
+
+class Claim:
+    @classmethod
+    def parse(cls, line):
+        words = line.split()
+        coords = words[2].split(",")
+        area = words[3].split("x")
+        return Claim(
+            int(words[0][1:]),
+            int(coords[0]),
+            int(coords[1][:-1]),
+            int(area[0]),
+            int(area[1]),
+        )
+
+    def __init__(self, id, x, y, w, h):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+        self.overlaps = False
+
+    def __repr__(self):
+        return f"Claim({self.id}, {self.x}, {self.y}, {self.width}, {self.height})"
+
+
+def part1(testing=False):
+    claims = _read_claims(testing, 1)
+    overlaps = {}
+    for claim in claims.values():
+        for i in range(claim.x, claim.x + claim.width):
+            for j in range(claim.y, claim.y + claim.height):
+                overlaps[(i, j)] = overlaps.get((i, j), 0) + 1
+    print(len([k for k, v in overlaps.items() if v > 1]))
+
+
+def part2(testing=False):
+    claims = _read_claims(testing, 1)
+    overlaps = {}
+    for claim in claims.values():
+        for i in range(claim.x, claim.x + claim.width):
+            for j in range(claim.y, claim.y + claim.height):
+                key = (i, j)
+                overlaps[key] = overlaps.get(key, [])
+                overlaps[key].append(claim.id)
+    for claim_ids in overlaps.values():
+        if len(claim_ids) > 1:
+            for claim_id in claim_ids:
+                claims[claim_id].overlaps = True
+    for claim in claims.values():
+        if not claim.overlaps:
+            print(claim.id)
+
+
+# Returns dict of {claim_id: claim}
+def _read_claims(testing=False, part_num=1):
+    claims = [Claim.parse(line) for line in data_file_lines("03", testing, part_num)]
+    return {claim.id: claim for claim in claims}
---
+++ python/y2018/template.py
@@ -0,0 +1,9 @@
+# Puzzle Name
+
+def part1(testing=False):
+    lines = data_file_lines("NN", testing, 1)
+    pass
+
+def part2(testing=False):
+    lines = data_file_lines("NN", testing, 1)
+    pass
