6f95073ab3413de14e613680b184640648a50b7b
Jim Menard <jim@jimmenard.com>
2018-12-06 12:00:05 -0500

    Python puzzle runner command line improvements

--- python/aoc.py
+++ python/aoc.py
@@ -4,17 +4,26 @@
 #
 # 't' for test
 
+import argparse
+import datetime
 import importlib
 import os.path
 import sys
 
-if len(sys.argv) < 4:
-    print("usage: aoc.py year day 1|2 [t]")
-    exit(1)
-year = int(sys.argv[1])
-day = int(sys.argv[2])
-part_num = int(sys.argv[3])
-testing = len(sys.argv) >= 5
+now = datetime.datetime.today()
+year = now.year
+day = now.day
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-y", "--year", help="year")
+parser.add_argument("-d", "--day",  help="day")
+parser.add_argument("-t", "--test", help="test mode", action="store_true")
+parser.add_argument("part_number", help="puzzle part number (1 or 2)")
+args = parser.parse_args()
+if args.year:
+    year = int(args.year)
+if args.day:
+    day = int(args.day)
 
 module = importlib.import_module(f"y{year}.day{'%02d' % day}")
-getattr(module, f"part{part_num}")(testing)
+getattr(module, f"part{int(args.part_number)}")(args.test)
