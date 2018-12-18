8fb066f72f404c36a46c961ae8860dad707dbcf8
Jim Menard <jim@jimmenard.com>
2018-12-18 10:44:55 -0500

    day 17 snapshot

---
+++ python/y2018/day17.py
@@ -0,0 +1,176 @@
+# Reservoir Research
+
+import re
+
+from utils import *
+
+from .world import Point, Thing, World
+
+
+class WaterWorld(World):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.min_y = 9999
+        self.max_y = 0
+        self.min_x = 9999
+        self.max_x = 0
+        self.flowing_water = []
+        self.resting_water = []
+        self.faucet = None
+
+    def move(
+        self, thing, from_loc, to_loc, ensure_empty=False, ignore_out_of_bounds=False
+    ):
+        super().move(
+            thing,
+            from_loc,
+            to_loc,
+            ensure_empty=ensure_empty,
+            ignore_out_of_bounds=ignore_out_of_bounds,
+        )
+        if type(thing) == Clay:
+            if to_loc.y < self.min_y:
+                self.min_y = to_loc.y
+            if to_loc.y > self.max_y:
+                self.max_y = to_loc.y
+            if to_loc.x - 1 < self.min_x:
+                self.min_x = to_loc.x - 1
+            if to_loc.x + 1 > self.max_x:
+                self.max_x = to_loc.x + 1
+
+    def water_stopped_flowing(self, water):
+        self.resting_water.append(water)
+        self.flowing_water.remove(water)
+
+    def reachable_tiles(self):
+        turn = 0
+        prev_count = -1
+        while True:
+            for w in self.flowing_water[:]:
+                w.flow()
+            w = Water(self)
+            self.move(w, None, Point(self.faucet.loc.x, self.faucet.loc.y + 1))
+            self.flowing_water.append(w)
+            self.print_map(turn)  # DEBUG
+            pause()  # DEBUG
+            count = len(self.resting_water) + len(
+                [
+                    w
+                    for w in self.flowing_water
+                    if w.loc.y in range(self.min_y, self.max_y + 1)
+                ]
+            )
+            if count == prev_count:
+                return count
+            prev_count = count
+            turn += 1
+
+    def print_map(self, turn=None):
+        if turn:
+            print()
+            print(f"After {turn} turns:")
+        for y in range(self.min_y, self.max_y + 1):
+            for x in range(self.min_x, self.max_x + 1):
+                print(self.char_at(x, y), end="")
+            print()
+
+
+class Clay(Thing):
+    def __init__(self, world):
+        super().__init__(world)
+        self.char = "#"
+
+
+# TODO when drops water, start y is one less than min y of any clay
+class Faucet(Thing):
+    def __init__(self, world):
+        super().__init__(world)
+        self.char = "+"
+
+    def flow(self):
+        pass
+
+
+class Water(Thing):
+    def __init__(self, world):
+        super().__init__(world)
+        self.char = "|"
+        self.can_flow = True
+
+    # If new water created, returns it
+    def flow(self):
+        if not self.can_flow:
+            return
+
+        start_loc = Point(self.loc.x, self.loc.y)
+
+        p = Point(self.loc.x, self.loc.y + 1)
+        if self.world.at(p) is None:
+            self.move(p, ignore_out_of_bounds=True)
+            self.char = "|"
+            return
+        self.char = "~"
+
+        # if below left and right are both already taken, can't move
+        if (
+            type(self.world.at(start_loc.x - 1, start_loc.y + 1)) == Water
+            and type(self.world.at(start_loc.x + 1, start_loc.y + 1)) == Water
+        ):
+            self.can_flow = False
+            self.world.water_stopped_flowing(self)
+            return
+
+        moved_left = False
+        p = Point(start_loc.x - 1, start_loc.y)
+        if self.world.at(p) is None:
+            self.move(p, ignore_out_of_bounds=True)
+            moved_left = True
+
+        p = Point(start_loc.x + 1, start_loc.y)
+        if self.world.at(p) is None:
+            if moved_left:
+                w = Water(self.world)
+                self.world.move(w, None, p)
+            else:
+                self.move(p, ignore_out_of_bounds=True)
+        else:
+            if not moved_left:
+                self.can_flow = False
+
+
+def part1(testing=False):
+    world = _read_world(1, testing)
+    if testing:
+        world.print_map()
+    print(world.reachable_tiles())
+
+
+def part2(testing=False):
+    world = _read_world(2, testing)
+
+
+def _read_world(part_num, testing):
+    soundings = []
+    for line in data_file_lines(17, part_num, testing):
+        print(line)  # DEBUG
+        m = re.match(r"x=(\d+), y=(\d+)\.\.(\d+)", line)
+        if m:
+            soundings.append((int(m[1]), int(m[1]), int(m[2]), int(m[3])))
+        else:
+            m = re.match(r"y=(\d+), x=(\d+)\.\.(\d+)", line)
+            soundings.append((int(m[2]), int(m[3]), int(m[1]), int(m[1])))
+    print(soundings)  # DEBUG
+    x_min = min(soundings, key=lambda s: s[0])[0] - 1
+    x_max = max(soundings, key=lambda s: s[1])[1] + 1
+    y_min = min(soundings, key=lambda s: s[2])[2] - 1
+    y_max = max(soundings, key=lambda s: s[3])[3] + 1
+    world = WaterWorld(x_max + 1, y_max + 1)
+    for s in soundings:
+        for y in range(s[2], s[3] + 1):
+            for x in range(s[0], s[1] + 1):
+                world.move(Clay(world), None, Point(x, y))
+    faucet = Faucet(world)
+    world.move(faucet, None, Point(500, 0))
+    world.flowing_water.append(faucet)
+    world.faucet = faucet
+    return world
---
+++ python/y2018/world.py
@@ -0,0 +1,104 @@
+# 2D generic world
+
+import collections
+
+Point = collections.namedtuple("Point", ["x", "y"])
+
+
+class Thing:
+    def __init__(self, world, loc=Point(0, 0)):
+        self.world = world
+        self.loc = loc
+        self.char = "."
+
+    def move(self, x, y=None, ignore_out_of_bounds=False):
+        p = Point(x, y) if y else x
+        self.world.move(
+            self,
+            self.loc,
+            p,
+            ensure_empty=False,
+            ignore_out_of_bounds=ignore_out_of_bounds,
+        )
+
+    def __str__(self):
+        return f"{self.char}@({self.loc.x},{self.loc.y})"
+
+
+class World:
+    """A 2D world. One thing at a time in each location."""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.map = [[None] * width for _ in range(height)]
+        self.turn = 0
+
+    def move(
+        self, thing, from_loc, to_loc, ensure_empty=False, ignore_out_of_bounds=False
+    ):
+        """Moves thing from one loc to another.
+
+        Does not change things's coords. Thing needs to do that.
+
+        `from_loc` may be None, in which case we must be placing the thing
+        for the first time.
+        """
+        if ensure_empty and self.at(to_loc) is not None:
+            raise Exception(
+                f"to_loc {to_loc} already occupied by #{self.char_at(to_loc)}"
+            )
+        if from_loc:
+            self.clear(from_loc)
+        if getattr(thing, "loc", None):
+            thing.loc = to_loc
+        if ignore_out_of_bounds and not self.in_bounds(to_loc):
+            return
+        self.map[to_loc.y][to_loc.x] = thing
+
+    def at(self, x, y=None):
+        if y is None:
+            y = x.y
+            x = x.x
+        if not self.in_bounds(x, y):
+            return None
+        return self.map[y][x]
+
+    def char_at(self, x, y=None):
+        thing = self.at(x, y)
+        if thing is None:
+            return "."
+        elif type(thing) == str:
+            return thing[0]
+        elif issubclass(thing, Thing):
+            return thing.char
+        else:
+            return "?"
+
+    def is_empty(self, x, y=None):
+        if y is None:
+            y = x.y
+            x = x.x
+        return self.map[y][x] is None
+
+    def clear(self, x, y=None):
+        if y is None:
+            y = x.y
+            x = x.x
+        if self.in_bounds(x, y):
+            self.map[y][x] = None
+
+    def in_bounds(self, x, y=None):
+        if y is None:
+            y = x.y
+            x = x.x
+        return y >= 0 and y < self.height and x >= 0 and x < self.width
+
+    def print_map(self, turn=None):
+        if turn:
+            print()
+            print(f"After {turn} turns:")
+        for y in range(self.height):
+            for x in range(self.width):
+                print(self.char_at(x, y), end="")
+            print()
