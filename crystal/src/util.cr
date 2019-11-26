470a3a2390d1750f62524c575c6ee5d104494982
Jim Menard <jim@jimmenard.com>
2019-11-26 15:39:04 -0500

    data_file_lines skips blank lines

--- crystal/src/util.cr
+++ crystal/src/util.cr
@@ -1,9 +1,9 @@
 class Util
-  def self.data_file_lines(year: Int32, day: Int32, part_num: Int32, testing: Bool)
+  def self.data_file_lines(year: Int32, day: Int32, part_num: Int32, testing: Bool) : Array(String)
     fname = "day"
     fname += "_test" if testing
     fname += ".txt"
     path = "data/y#{year}/day#{%02d % day}#{testing ? '_test' : ''}"
-    File.read_lines(path)
+    File.read_lines(path).delete("")
   end
 end
