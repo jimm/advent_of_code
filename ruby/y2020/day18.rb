09896d7b53fa65f51c489d8570bbc812950e310a
Jim Menard <jim.menard@warbyparker.com>
2020-12-18 08:46:43 -0500

    day 18 solution

---
+++ ruby/y2020/day18.rb
@@ -0,0 +1,83 @@
+# Operation Order
+
+class Day18 < Day
+  def part1
+    do_part(:next_op_index_equal_precedence)
+  end
+
+  def part1_tests
+    do_tests(:next_op_index_equal_precedence)
+  end
+
+  def part2
+    do_part(:next_op_mult_high_precedence)
+  end
+
+  def part2_tests
+    do_tests(:next_op_mult_high_precedence)
+  end
+
+  def do_part(next_op_index_func)
+    total =
+      data_lines(1)
+        .map { |line| new_math_eval(parse(line), next_op_index_func) }
+        .sum
+    puts(total)
+  end
+
+  def do_tests(next_op_index_func)
+    errors = []
+    test_chunks.each do |expected, lines|
+      expected = expected.to_i
+      answer = new_math_eval(parse(lines[0]), next_op_index_func)
+      if answer != expected
+        print('F')
+        errors << "expected #{expected} got #{answer}"
+      else
+        print('.')
+      end
+    end
+    puts()
+    if errors.empty?
+      puts('ok')
+    else
+      errors.each { |err| puts(err) }
+    end
+  end
+
+  def new_math_eval(expr, next_op_index_func)
+    return expr if Integer === expr
+    return new_math_eval(expr[0], next_op_index_func) if expr.length == 1
+
+    op_index = send(next_op_index_func, expr)
+    left_val = new_math_eval(expr[op_index - 1], next_op_index_func)
+    op = expr[op_index]
+    right_val = new_math_eval(expr[op_index + 1], next_op_index_func)
+    expr[op_index-1, 3] = left_val.send(op, right_val)
+    new_math_eval(expr, next_op_index_func)
+  end
+
+  def next_op_index_equal_precedence(expr)
+    i1 = expr.index(:+)
+    i2 = expr.index(:*)
+    return i2 if i1.nil?
+    return i1 if i2.nil?
+    return i1 if i1 < i2
+    i2
+  end
+
+  def next_op_mult_high_precedence(expr)
+    i = expr.index(:+)
+    return i if i
+    expr.index(:*)
+  end
+
+  def parse(line)
+    array_innards = line
+                      .gsub(/\(/, '[')
+                      .gsub(/\)/, ']')
+                      .gsub(/([\+\*])/, ':\1')
+                      .gsub(/ /, ',')
+    eval('[' + array_innards + ']')
+  end
+end
