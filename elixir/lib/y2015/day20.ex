1cb963772a1d581d3dbf41dba6136a3771851dc2
Jim Menard <jim@jimmenard.com>
2021-01-22 08:58:56 -0500

    2015 day 20 part 2

--- elixir/lib/y2015/day20.ex
+++ elixir/lib/y2015/day20.ex
@@ -8,44 +8,52 @@ defmodule Y2015.Day20 do
   def run1 do
     # sum of factors of house number >= @puzzle_input / 10
     num_houses_below = Stream.iterate(1, &(&1 + 1))
-    |> Stream.map(&sum_of_factors/1)
+    |> Stream.map(&factors/1)
+    |> Stream.map(&Enum.sum/1)
     |> Stream.take_while(&(&1 < @puzzle_input / 10))
     |> Enum.count()
     num_houses_below + 1
   end
 
-  # 776160 is too low
   def run2 do
-    # sum of factors of house number >= @puzzle_input / 11
     num_houses_below = Stream.iterate(1, &(&1 + 1))
-    |> Stream.map(&sum_of_factors/1)
-    |> Stream.take_while(&(&1 < @puzzle_input / 11))
+    |> Stream.map(&factors/1)
+    |> Stream.map(&num_presents_max_50_visits_11_each/1)
+    |> Stream.take_while(&(&1 < @puzzle_input))
     |> Enum.count()
     num_houses_below + 1
   end
 
-  # Returns a Stream of tuples with house numbers and present counts.
-  defp sum_of_factors(1) do
-    1
+  defp factors(1) do
+    [1]
   end
-  defp sum_of_factors(n) do
+  defp factors(n) do
     sqrt_n = round(:math.sqrt(n))
-    sum_of_factors(n, [1, n], 2, sqrt_n)
+    factors(n, [1, n], 2, sqrt_n)
   end
-  defp sum_of_factors(_n, factors, i, sqrt_n) when i > sqrt_n do
-    Enum.sum(factors)
+  defp factors(_n, factors, i, sqrt_n) when i > sqrt_n do
+    factors
   end
-  defp sum_of_factors(n, factors, i, sqrt_n) do
+  defp factors(n, factors, i, sqrt_n) do
     if rem(n, i) == 0 do
-      other = n / i
+      other = round(n / i)
       new_factors = if i == other do
         [i | factors]
       else
         [other | [i | factors]]
       end
-      sum_of_factors(n, new_factors, i+1, sqrt_n)
+      factors(n, new_factors, i+1, sqrt_n)
     else
-      sum_of_factors(n, factors, i+1, sqrt_n)
+      factors(n, factors, i+1, sqrt_n)
     end
+  end
+
+  defp num_presents_max_50_visits_11_each(factors) do
+    n = Enum.max(factors)
+    factors
+    |> Enum.map(fn fac ->
+      if (fac * 50) >= n, do: (fac * 11), else: 0
+    end)
+    |> Enum.sum
   end
 end
