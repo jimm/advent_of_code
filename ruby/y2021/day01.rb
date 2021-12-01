4006e9321dfab6fae1bc5a4ae9cdeb514adc3bd1
Jim Menard <jim@jimmenard.com>
2021-12-01 11:24:58 -0500

    refactor, puzzle name

--- ruby/y2021/day01.rb
+++ ruby/y2021/day01.rb
@@ -1,26 +1,23 @@
-# Puzzle Name
+# Sonar Sweep
 
 class Day01 < Day
   def part1
     nums = data_lines(1).map(&:to_i)
+    puts num_increases(nums)
+  end
+
+  def part2
+    nums = data_lines(1).map(&:to_i).each_cons(3).map(&:sum)
+    puts num_increases(nums)
+  end
+
+  def num_increases(nums)
     num_increases = 0
     prev = nums[0]
     nums[1..-1].each do |n|
       num_increases += 1 if n > prev
       prev = n
     end
-    puts num_increases
-  end
-
-  def part2
-    nums = data_lines(1).map(&:to_i)
-    num_increases = 0
-    prev = 9999
-    nums.each_cons(3) do |arr|
-      val = arr.sum
-      num_increases += 1 if val > prev
-      prev = val
-    end
-    puts num_increases
+    num_increases
   end
 end
