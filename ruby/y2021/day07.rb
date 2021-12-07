b9f08687db12c1ece9e260a5cfd7bdc072bcf6da
Jim Menard <jim@jimmenard.com>
2021-12-07 09:25:45 -0500

    day 7 refactor

--- ruby/y2021/day07.rb
+++ ruby/y2021/day07.rb
@@ -3,32 +3,32 @@
 class Day07 < Day
   def part1
     positions = data_lines(1).first.split(',').map(&:to_i)
-    min_used = min_fuel(positions) do |target|
-      positions.reduce(0) do |acc, pos|
-        acc + (target - pos).abs
-      end
-    end
+    min_used = min_fuel(positions) { |distance| distance }
     puts min_used
   end
 
   def part2
     positions = data_lines(1).first.split(',').map(&:to_i)
-    min_used = min_fuel(positions) do |target|
-      positions.reduce(0) do |acc, pos|
-        n = (target - pos).abs
-        acc + (n * n + n) / 2
-      end
+    min_used = min_fuel(positions) do |distance|
+      (distance * distance + distance) / 2
     end
     puts min_used
   end
 
-  def min_fuel(positions)
+  def min_fuel(positions, &block)
     min = positions.min
     max = positions.max
     used = []
     min.upto(max) do |pos|
-      used << yield(pos)
+      used << fuel_used(positions, pos, block)
     end
     used.min
+  end
+
+  def fuel_used(positions, target, block)
+    positions.reduce(0) do |acc, pos|
+      distance = (target - pos).abs
+      acc + block.call(distance)
+    end
   end
 end
