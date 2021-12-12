f88964d90e770adac6ad27684491215806f72d94
Jim Menard <jim@jimmenard.com>
2021-12-12 09:41:25 -0500

    day 12 part 1 solved, part 2 in progress

---
+++ ruby/y2021/day12.rb
@@ -0,0 +1,167 @@
+# Puzzle Name
+
+class Day12 < Day
+  class Cave
+    attr_reader :name, :size
+    attr_accessor :connections
+
+    def initialize(name)
+      @name = name
+      @size = name.downcase == name ? :small : :big
+      @connections = []
+    end
+
+    def start?
+      @name == 'start'
+    end
+
+    def end?
+      @name == 'end'
+    end
+
+    def add(cave)
+      # Don't add outgoing caves to end or incoming caves to start
+      return if end? || cave.start?
+
+      @connections << cave
+    end
+
+    def remove(cave)
+      @connections.delete(cave)
+    end
+
+    def remove_self
+      @connections.each do |c|
+        c.remove(self)
+      end
+      @connections = []
+    end
+
+    def dead_end?
+      @size == :small && @connections.all? { |c| c.size == :small }
+    end
+
+    def to_s
+      @name
+    end
+
+    def inspect
+      to_s
+    end
+  end
+
+  class CaveSystem
+    attr_reader :caves, :start, :end
+
+    def initialize(lines)
+      @caves = {}
+      lines.each do |line|
+        names = line.split('-')
+        c0 = caves[names[0]] || build_cave(names[0])
+        c1 = caves[names[1]] || build_cave(names[1])
+        c0.add(c1)
+        c1.add(c0)
+      end
+      prune_dead_ends
+    end
+
+    def num_paths(max_small_cave_revisits = 0)
+      paths = build_paths([
+                            [[@start], max_small_cave_revisits]
+                          ])
+      paths.size
+    end
+
+    private
+
+    # `paths` is an array of [path, max_small_cave_revisits] arrays. For
+    # each path, return all possible paths leading to @end from the last
+    # node in the path.
+    def build_paths(paths)
+      return paths.map(&:first) if paths.all? { |p| p[0].last == @end }
+
+      new_paths = []
+      paths.map do |path_and_mscr|
+        path, max_small_cave_revisits = *path_and_mscr
+        curr_cave = path.last
+        if curr_cave.end?
+          new_paths << path_and_mscr
+          next
+        end
+        curr_cave.connections.each do |c|
+          if c.size == :small && path.include?(c)
+            if max_small_cave_revisits > 0
+              # We have already visited this small cave, but we're allowed
+              # to revisit it.
+              new_paths << ([path + [c], max_small_cave_revisits - 1])
+            end
+          else
+            new_paths << ([path + [c], max_small_cave_revisits])
+          end
+        end
+      end
+      build_paths(new_paths)
+    end
+
+    def build_cave(name)
+      Cave.new(name).tap do |c|
+        @caves[name] = c
+        @start = c if name == 'start'
+        @end = c if name == 'end'
+      end
+    end
+
+    def prune_dead_ends
+      dead_ends = find_dead_ends
+      until dead_ends.empty?
+        dead_ends.each do |c|
+          c.remove_self
+          @caves.delete(c.name)
+        end
+        dead_ends = find_dead_ends
+      end
+    end
+
+    def find_dead_ends
+      @caves.values
+            .select { |c| c.dead_end? }
+            .reject { |c| %w[start end].include?(c.name) }
+    end
+  end
+
+  def part1
+    cave_system = CaveSystem.new(data_lines(1))
+    puts cave_system.num_paths
+  end
+
+  def part1_tests
+    run_chunk_tests(1) do |expected_str, lines|
+      expected = expected_str.split(',').first.to_i
+      cave_system = CaveSystem.new(lines)
+      num_paths = cave_system.num_paths
+      [num_paths == expected, num_paths]
+    end
+  end
+
+  def part1_test(lines)
+    expected = lines[0][5..-1].to_i
+    cave_system = build_caves(lines[1..-1])
+    num_paths = cave_system.num_paths
+
+    puts "expected #{expected}, got #{num_paths}" unless num_paths == expected
+  end
+
+  def part2
+    cave_system = CaveSystem.new(data_lines(1))
+    puts cave_system.num_paths(1)
+  end
+
+  def part2_tests
+    run_chunk_tests(1) do |expected_str, lines|
+      expected = expected_str.split(',').last.to_i
+      cave_system = CaveSystem.new(lines)
+      num_paths = cave_system.num_paths(1)
+      [num_paths == expected, num_paths]
+    end
+  end
+end
