8fd58e5cc541992020f4f21a170b30e55c652234
Jim Menard <jim@jimmenard.com>
2021-12-17 14:09:27 -0500

    day 17 part 2 solved

--- ruby/y2021/day17.rb
+++ ruby/y2021/day17.rb
@@ -1,5 +1,7 @@
 # Trick Shot
 
+require_relative '../utils'
+
 class Day17 < Day
   def part1
     lines = data_lines(1)
@@ -29,22 +31,42 @@ class Day17 < Day
       x_velocity_range = calc_x_range(target[:x])
       max_heights = x_velocity_range.map { |x| max_height(target, x) }
       val = max_heights.max
-      [val == expected.to_i, val]
+      [val == expected.split(',')[1].to_i, val]
     end
   end
 
+  # 396 is too low
   def part2
     lines = data_lines(1)
     target = read_target(lines)
 
-    max_heights = []
-    (-200..200).each do |x_velocity|
-      (-200..200).each do |y_velocity|
-        max_heights << max_height_for_trajectory(target, x_velocity, y_velocity)
+    vels = []
+    (0..(target[:x].max + 1)).each do |x_velocity|
+      y_bounds = target[:y].min - 1
+      y_bounds = y_bounds.abs if y_bounds < 0
+      (-y_bounds..y_bounds).each do |y_velocity|
+        vels << [x_velocity, y_velocity] if intersects?(target, x_velocity, y_velocity)
       end
     end
-    max_heights.compact!.uniq!
-    puts max_heights.length
+    puts vels.length
+  end
+
+  def part2_tests
+    run_chunk_tests(1) do |expected, lines|
+      target = read_target(lines)
+
+      vels = []
+      (0..(target[:x].max + 1)).each do |x_velocity|
+        y_bounds = target[:y].min - 1
+        y_bounds = y_bounds.abs if y_bounds < 0
+        (-y_bounds..y_bounds).each do |y_velocity|
+          vels << [x_velocity, y_velocity] if intersects?(target, x_velocity, y_velocity)
+        end
+      end
+
+      val = vels.length
+      [val == expected.split(',')[1].to_i, val]
+    end
   end
 
   def read_target(lines)
@@ -61,7 +83,22 @@ class Day17 < Day
   end
 
   def past_target?(x, y, target)
-    x > target[:x].max || y < target[:y].max
+    x > target[:x].max || y < target[:y].min
+  end
+
+  # 6, 0 and 7, -1 not being reported as on target
+  def intersects?(target, x_velocity, y_velocity)
+    x = 0
+    y = 0
+    while 1
+      return true if on_target?(x, y, target)
+      return false if past_target?(x, y, target)
+
+      x += x_velocity
+      y += y_velocity
+      x_velocity -= 1 if x_velocity > 0
+      y_velocity -= 1
+    end
   end
 
   def max_height(target, x_velocity)
