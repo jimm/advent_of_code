0782c2e1ac926b8f04a892113d02fe5657d5d2d3
Jim Menard <jim@jimmenard.com>
2021-12-26 09:09:10 -0500

    day 25 part 1 solved

---
+++ ruby/y2021/day25.rb
@@ -0,0 +1,84 @@
+# Sea Cucumber
+
+require_relative '../map'
+
+class Day25 < Day
+  class SeaCucumberMap < Map
+    attr_reader :num_moved
+
+    def step
+      @num_moved = 0
+      copy = dup
+
+      # move horizontal cukes
+      @height.times.each do |row|
+        (@width - 1).downto(0).each do |col|
+          next unless at(row, col) == '>' && at(row, col + 1) == '.'
+
+          copy.set(row, col, '.')
+          copy.set(row, col + 1, '>')
+          @num_moved += 1
+        end
+      end
+      @cells = copy.cells
+      copy = dup
+
+      # move vertical cukes
+      (@height - 1).downto(0).each do |row|
+        @width.times.each do |col|
+          next unless at(row, col) == 'v' && at(row + 1, col) == '.'
+
+          copy.set(row, col, '.')
+          copy.set(row + 1, col, 'v')
+          @num_moved += 1
+        end
+      end
+
+      @cells = copy.cells
+    end
+
+    def column(n)
+      @cells.map { |row| row[n] }
+    end
+
+    def dup
+      duplicate = super
+      duplicate.instance_variable_set(:@cells, duplicate.cells.dup)
+      (0..height - 1).each do |row|
+        duplicate.cells[row] = duplicate.cells[row].dup
+      end
+      duplicate
+    end
+  end
+
+  def part1
+    lines = data_lines(1)
+    map = SeaCucumberMap.new(lines, :both)
+    step = 0
+    while true
+      step += 1
+      map.step
+      break if map.num_moved == 0
+    end
+    puts step
+  end
+
+  def part1_tests
+    run_chunk_tests(1) do |expected, lines|
+      expected = eval(expected)
+      map = SeaCucumberMap.new(lines, :both)
+      step = 0
+      while true
+        step += 1
+        map.step
+        break if map.num_moved == 0
+      end
+      pp expected
+      [step == expected[:stop_step], step]
+    end
+  end
+
+  def part2
+    lines = data_lines(1)
+  end
+end
