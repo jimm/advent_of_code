4c8871d7365e48be797cb2132b1f6ee21f7acbe8
Jim Menard <jim@jimmenard.com>
2022-12-06 08:20:02 -0500

    day 6 solved

--- ruby/y2022/day06.rb
+++ ruby/y2022/day06.rb
@@ -1,6 +1,8 @@
-PUZZLENAME
+# Tuning Trouble
 
-class DAYNAME < Day
+class Day06 < Day
+  PART_PATTERN_LENGTH = [4, 14]
+
   def part1
     puts do_part(data_lines(1))
   end
@@ -21,13 +23,17 @@ class DAYNAME < Day
 
   def do_tests
     run_chunk_tests(1) do |expected, lines|
-      expected = expected.split(',')[@part_number - 1]
+      expected = expected.split(',')[@part_number - 1].to_i
       answer = do_part(lines)
       [answer == expected, answer, expected]
     end
   end
 
   def do_part(lines)
-    # TODO
+    chars = lines.first.split('')
+    n = PART_PATTERN_LENGTH[@part_number - 1]
+    (0..chars.length - n).each do |i|
+      return i + n if chars[i, n].uniq.length == n
+    end
   end
 end
